0fceebf136758edbf50a16b500e014ade801713e
Norm O'Hagan <whosnorman@gmail.com>
2022-07-27 14:54:22 -0400

    try using httpolyglot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+const httpolyglot = require('httpolyglot');
 const app = require('./app');
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening on port ${port}`);
-});
+const options = {};
+httpolyglot.createServer(options, app).listen(port);
+
+// app.listen(port, () => {
+//   /* eslint-disable no-console */
+//   console.log(`Listening on port ${port}`);
+// });
